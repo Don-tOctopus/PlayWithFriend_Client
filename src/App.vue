eefe9f75d541200221b295b71721ad4eba4aa9e8
sudhdkso <waga1009@gmail.com>
2022-09-22 13:35:49 +0900

    [#1] feat: 사이드 바 추가

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,26 @@
-<template>  
-  <v-app>
-    <v-main>
-      <router-view/>
-    </v-main>
-  </v-app>
+<template>
+  <v-row>
+    <v-col cols="4">
+      <SideMenu> </SideMenu>
+    </v-col>
+    
+    <v-col cols="8">
+      <v-app>
+        <v-main>
+          <router-view/>
+        </v-main>
+      </v-app>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-
+import SideMenu from './components/SideMenu'
 export default {
   name: 'App',
-
+  components : {
+    SideMenu
+  },
   data: () => ({
     //
   }),
