702615b26585d19162e4233778b7f0b1acb1b70a
uzhjd <73466440+uzhjd@users.noreply.github.com>
2022-09-26 00:39:42 +0900

    [#7] feat: favicon제작

--- src/views/VideoRoomCreation.vue
+++ src/views/VideoRoomCreation.vue
@@ -2,16 +2,12 @@
   <div class="container">
     <v-card id="videoRoomCard">
         <v-card-title class="title">화상채팅 방 생성</v-card-title>
-        <!-- <div class="sub-title">
-          채팅방을 생성하여 방장으로 참가합니다.<br />채팅방 이름과 입장
-          비밀번호를 설정해주세요.
-        </div> -->
 
         <v-card-text class="creation-room-container">
           <div class="creation-room-box">
             <!-- id -->
             <div class="form">
-              <p class="fromTitle">방명</p>
+              <p class="formTitle">방명</p>
               <v-text-field
                 label="채팅방 이름를 설정해주세요."
                 :rules="rules"
@@ -20,7 +16,7 @@
             </div>
             <!-- pw -->
             <div class="form">
-              <p class="fromTitle">입장 비밀번호</p>
+              <p class="formTitle">입장 비밀번호</p>
               <v-text-field
                 label="입장 비밀 번호를 설정해주세요."
                 :rules="rules"
@@ -29,7 +25,7 @@
             </div>
             <!-- 인원 설정 -->
             <div class="form">
-              <p class="fromTitle">인원 설정</p>
+              <p class="formTitle">인원 설정</p>
               <v-select
                 :items="items"
                 label="참여 인원 수"
@@ -37,14 +33,13 @@
             </div>
             <!-- 친구 초대 -->
             <div class="form">
-              <p class="fromTitle">친구 초대</p>
+              <p class="formTitle">친구 초대</p>
               <input
                   class="creation-room-form__input"
                   placeholder="친구의 ID를 콤마(,)로 구분해서 입력해주세요(EX. aaa,bbb,ccc)"
               />  
             </div>
 
-            <br>
             <br>
             <br>
             <v-btn
@@ -91,7 +86,7 @@ export default {
 .videoRoomCard {
   border: #818F59;
 }
-.fromTitle {
+.formTitle {
   font-size: 16px;
 }
 .form {
